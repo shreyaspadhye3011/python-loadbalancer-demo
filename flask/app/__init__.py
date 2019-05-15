9c4a7cfe8bd56e4172550c9dbf56614a206b5986
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-05-14 20:17:10 -0700

    Added Mongo connectivity code & tested

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-print("##### Inside init")
+
+app.config['MONGO_URI'] = "mongodb://mongo:27017/myusers"
+mongo = PyMongo(app)
 
 from app import views
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,15 +1,19 @@
 
-from app import app
+from app import app, mongo
 from flask import request
 import os
 
 @app.route("/")
 def index():
     app_name = os.getenv("APP_NAME")
+    port = os.getenv("FLASK_PORT")
     if app_name:
         if request.args and ('user_id' in request.args):
             user_id = request.args.get('user_id')
-            return user_id
-            # return "Logic to get keys under development"
-        return "Currently Serving: {}".format(app_name)
+            # TODO: plug in code to fetch key value from mongodb
+            test_user = mongo.db.data.find({"user_id": 1})
+            print("*********")
+            print(type(test_user))
+            return "user_id: {}, port: {}".format(user_id, port)
+        return "Currently Serving: {} from port: {}".format(app_name, port)
     return "Flask: Hello"
