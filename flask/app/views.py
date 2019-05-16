2eebfc044fc84fe453db8dd285d4f9da763b07f4
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-05-16 09:28:44 -0700

    improved 400 response and only kept error message

--- flask/app/views.py
+++ flask/app/views.py
@@ -9,14 +9,15 @@ def index():
     port = os.getenv("FLASK_PORT")
     if app_name:
         response = {}
-        response['port'] = port
-        response['user_key'] = None
         if request.args and ('user_id' in request.args):
             user_id = request.args.get('user_id')
             user_key_cursor = mongo.db.data.find({"user_id": user_id})
             if user_key_cursor.count() > 0:
+                response['port'] = port
+                response['user_key'] = None
                 for item in user_key_cursor:
                     response['user_key'] = item['user_key']
                 return jsonify(response), 200
+        response['error'] = "user_id does not exist"
         return jsonify(response), 400
     return "Flask: Hello"
